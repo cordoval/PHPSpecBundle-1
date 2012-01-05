64440a8664eb3d09b9710e687fdad6b402438510
Luis Cordova <cordoval@gmail.com>
2012-01-05 08:01:04 -0500

    working on specing command

--- Spec/Command/GenerateViewSpecCommandSpec.php
+++ Spec/Command/GenerateViewSpecCommandSpec.php
@@ -11,9 +11,39 @@ class DescribeGenerateViewSpecCommand extends \PHPSpec\Context
 
     }
 
-    public function itShouldCheckIfSpecViewDirectoryExists()
+    public function itShouldGenerateViewInLineParameters()
     {
+        $client = self::createClient();
+        $output = $this->runCommand($client, "phpspec:generate:view-spec name-of-bundle:name-of-controller:name-of-view");
 
+        $generator = $client->getKernel()->getContainer()->get('phpspec_generator');
+
+        $generator->generateViewFor($bundleName, $controllerName, $viewName);
+
+        $this->assertContains('view generated', $output);
     }
 
+    /**
+     * Runs a command and returns it output
+     */
+    public function runCommand(Client $client, $command)
+    {
+        $application = new Application($client->getKernel());
+        $application->setAutoExit(false);
+
+        $fp = tmpfile();
+        $input = new StringInput($command);
+        $output = new StreamOutput($fp);
+
+        $application->run($input, $output);
+
+        fseek($fp, 0);
+        $output = '';
+        while (!feof($fp)) {
+            $output = fread($fp, 4096);
+        }
+        fclose($fp);
+
+        return $output;
+    }
 }
